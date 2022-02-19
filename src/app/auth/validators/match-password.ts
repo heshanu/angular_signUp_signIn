91db9d2d6cacb8b4fecece5852809b5c83ef0b1e
heshan umayanga <heshanu97@gmail.com>
2022-02-19 23:56:38 +0530

    bug fixed

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,3 +1,14 @@
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { Injectable } from "@angular/core";
+@Injectable({providedIn:'root'})
 export class MatchPassword {
-    
+    validate(formGroup:AbstractControl):ValidationErrors|null{
+        const {password,passwordConformation} =formGroup.value;
+        if(password===passwordConformation){
+            return null;
+        }else{
+            return {passwordDontMatch:true};
+        }
+    }
 }
+
