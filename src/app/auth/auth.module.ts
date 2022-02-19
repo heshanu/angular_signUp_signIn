8abe8b784136178aad8019a4b3c25e11534d7ad3
heshan umayanga <heshanu97@gmail.com>
2022-02-19 23:05:13 +0530

    solve bug

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import { MatchPasswordComponent } from './validators/match-password/match-password.component';
 
 @NgModule({
   declarations: [
     SigninComponent,
-    SignupComponent,
-    MatchPasswordComponent
+    SignupComponent
   ],
   imports: [
     CommonModule,
